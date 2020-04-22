83fd41afb58c6a333e5f57b985e60eebc1907f95
adriank24 <41040039+adriank24@users.noreply.github.com>
2020-04-22 14:56:44 +0700

    fixing minor

--- src/main/java/MockSpotify/Spotify/repositories/SongRepository.java
+++
@@ -1,8 +0,0 @@
-package MockSpotify.Spotify.repositories;
-
-import MockSpotify.Spotify.entities.Song;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SongRepository extends CrudRepository<Song,Integer> {
-
-}
