1f1c4a25e84df4c80c107acbedd53de69fb62734
adriank24 <41040039+adriank24@users.noreply.github.com>
2020-04-25 11:57:17 +0700

    label controller

--- src/main/java/com/example/Spotify/controller/LabelController.java
+++ src/main/java/com/example/Spotify/controller/LabelController.java
@@ -12,13 +12,105 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.Spotify.entities.User;
-import com.example.Spotify.entities.Genre;
+import com.example.Spotify.entities.Artist;
+import com.example.Spotify.entities.Song;
 import com.example.Spotify.entities.Label;
-import com.example.Spotify.repositories.UserRepository;
-import com.example.Spotify.repositories.GenreRepository;
+import com.example.Spotify.repositories.ArtistRepository;
+import com.example.Spotify.repositories.SongRepository;
 import com.example.Spotify.repositories.LabelRepository;
 
+@Controller
+@RequestMapping(path="/spotif")
 public class LabelController {
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private SongRepository songRepository;
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    Label temp = new Label();
+
+    @PostMapping(path="/label/add")
+    public @ResponseBody ResponseEntity<Label> addLabel(@RequestParam String labelName,
+    @RequestParam Integer labelId){
+        try{
+            Label labelData=new Label();
+            labelData.setName(labelName);
+            labelData.setId(labelId);
+            labelRepository.save(labelData);
+            return ResponseEntity.ok(labelData);
+        }catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(path="/label/get")
+    public @ResponseBody ResponseEntity<Iterable<Label>> getAllLabel(){
+        try{
+            return ResponseEntity.ok(labelRepository.findAll());
+        }
+        catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping(path="/label/get/{id}")
+    public @ResponseBody ResponseEntity<Label> getLabel(@PathVariable int labelId){
+        try{
+            Label labelData = labelRepository.findById(labelId).get();
+            return ResponseEntity.ok(labelData);
+        }
+        catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping(path="/label/update/{id}")
+    public @ResponseBody ResponseEntity<Label> updateLabel(@PathVariable int id, @RequestParam String name) {
+        try {
+        Label labelData = new Label();
+        labelData.setId(id);
+        labelData.setName(name);
+        labelRepository.save(labelData);
+        return ResponseEntity.ok(labelData);   
+          }catch (Exception e) {
+            return ResponseEntity.notFound().build();
+          }
+    }
+
+
+    // @GetMapping(path="/playlist/get/{email}")
+    // public @ResponseBody ResponseEntity<List<Playlist>> getPlaylistUser(@PathVariable String email){
+    //     try{
+    //         List<User> user = userRepository.findByEmail(email);
+    //         Integer id_user=user.get(0).getId();
+    //         List<Playlist> playlistData = playlistRepository.findByUser(id_user);
+    //         return ResponseEntity.ok(playlistData);
+    //     }catch(Exception e){
+    //         return ResponseEntity.notFound().build();
+    //     }
+    // }
+
+
+
+    // @DeleteMapping(path = "/playlist/delete/{namePlaylist}")
+    // public @ResponseBody ResponseEntity<Void> deletePlaylist(@PathVariable String namePlaylist){
+    //     try {
+    //         List<Playlist> playlist = playlistRepository.findByName(namePlaylist);
+    //         Integer id_playlist=playlist.get(0).getId();
+    //         List<PlaylistSong> playlistSong = playlistSongRepository.findByPlaylistId(id_playlist);
+
+    //         for(int i=0; i<playlistSong.size();i++){
+    //             playlistSongRepository.deleteById(playlistSong.get(i).getId());
+    //         }
+    //         playlistRepository.deleteById(id_playlist);
+    //         return ResponseEntity.ok().build();
+    //     }catch (Exception e){
+    //         return ResponseEntity.notFound().build();
+    //     }
+    // }
 
 }
